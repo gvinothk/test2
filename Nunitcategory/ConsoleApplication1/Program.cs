e1c2810a96fe88616f812c6f8e8f3f6d62b71719
gvinothk <gvinothk@ilink-systems.com>
2016-03-31 11:55:11 +0530

    test2

---
+++ Nunitcategory/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nunitcategory;
+
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nunitcategory.TestClass t1 = new Nunitcategory.TestClass();
+            
+        }
+    }
+}
---
+++ Nunitcategory/Nunitcategory/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using NUnit;
+//using Nunitcategory;
+//using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Chrome;
+
+using MbUnit;
+using MbUnit.Core;
+using MbUnit.Framework;
+
+
+namespace Nunitcategory
+{
+    [TestFixture]
+    [Category("MathS")]
+    public class TestClass
+    {
+        [Test]
+        [Category("Rediff"), Row ("http://www.rediff.com")]
+        [Category("google"), Row("http://www.google.com")]
+        public void AddTest(string url)
+        {
+            //IWebDriver dr = new ChromeDriver(@"d:\chromedriver.exe");
+
+            //dr.Navigate().GoToUrl(url);
+            Console.WriteLine(url);
+            Console.WriteLine("test");
+            Console.ReadLine();
+            
+        }
+
+        [Test]
+        [Category("test"), Row("http://www.rediff.com")]        
+        public void AddTest1(string url)
+        {
+            //IWebDriver dr = new ChromeDriver(@"d:\chromedriver.exe");
+
+            //dr.Navigate().GoToUrl(url);
+            Console.WriteLine(url);
+            Console.ReadLine();
+
+        }
+
+        [Test]
+        [Category("test"), Row("http://www.rediff.com")]
+        public void AddTest2(string url)
+        {
+            //IWebDriver dr = new ChromeDriver(@"d:\chromedriver.exe");
+
+            //dr.Navigate().GoToUrl(url);
+            Console.WriteLine(url);
+            Console.ReadLine();
+
+        } 
+    }
+}
